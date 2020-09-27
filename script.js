5555679d9661bee76c0068d6c23f45d710499eca
Shakana Fox <foxshakana@gmail.com>
2020-09-27 08:51:50 -0400

    created quiz statements

--- script.js
+++ script.js
@@ -14,8 +14,63 @@
 
 var challengeContainer = document.getElementById("challenge");
 var startButton = document.getElementById("start-quiz");
+var answerOptions = document.getElementById("answer-options");
+
+//quiz statements
+var statements = [
+    {
+        Statement: "Arrays in Javascript can be used to store ____."
+
+        answerOptions: "1. numbers and strings", "2. other arrays", "3. boolean", "4. all of the above"
+
+    }, 
+    {
+        Statement: "Commonly used data types DO NOT include:"
+
+        answerOptions: "1. strings", "2. booleans", "3. alerts", "4. numbers"
+   
+    }
+    {
+        Statement: "A very useful tool used during development and debugging for printing content to the debugger is:"
+
+        answerOptions: "1. JavaScript", "2. terminal / bash", "3. for loops", "4. console.log"   
+    }
+    {
+        Statement: "String values must be enclosed within _____ when being assigned to variables."
+
+        answerOptions: "1. commas", "2. curly brackets", "3. quotas", "4. parentheses"
+   
+    }
+]
+ //     //
+    //     Statement: "Arrays in Javascript can be used to store ____."
+
+    //     optionA: ""
+
+    //     optionB: ""
+
+    //     optionC: ""
+
+    //     optionD: ""
+    // },
+
+function renderAnswerOptions() {
+    var availableAnswers = ["strings", "booleans", "alerts", "numbers"];
+
+    for (var i = 0; i <availableAnswers.length; i++){
+        //1. Create an element.
+        var button = document.createElement("button");
+        //2.add content
+        button.textContent = availableAnswers[i];
+        button.setAttribute("data-value", availableAnswers[i]);
+        //3. Append to an existing elememt.
+        quizOptions.append(button);
+
+    }
+}
 
 
 startButton.addEventListener("click", function() {
     challengeContainer.style.display = "none";
+    renderAnswerOptions();
 });
