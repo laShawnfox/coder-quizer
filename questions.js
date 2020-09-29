5f747f7f9264ec4a79dcffdb47e0576609c7a0cc
Shakana Fox <foxshakana@gmail.com>
2020-09-29 14:21:29 -0400

    updated question corrcet answers

---
+++ questions.js
@@ -0,0 +1,38 @@
+var questionsArr = [
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+
+        optionA: "1. numbers and strings",
+        optionB: "2. other arrays", 
+        optionC: "3. boolean", 
+        optionD: "4. all of the above",
+        correct : "optionD"
+    }, 
+    {
+        question: "Commonly used data types DO NOT include:",
+
+        optionA: "1. strings", 
+        optionB:"2. booleans",
+        optionC: "3. alerts", 
+        optionD:"4. numbers",
+        correct : "optionC"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+
+        optionA: "1. JavaScript",
+        optionB: "2. terminal / bash", 
+        optionC: "3. for loops", 
+        optionD:"4. console.log",  
+        correct :  "optionD"
+    },
+    
+    {   question: "String values must be enclosed within _____ when being assigned to variables.",
+
+        optionA: "1. commas", 
+        optionB:"2. curly brackets", 
+        optionC:"3. quotas", 
+        optionD:"4. parentheses",
+        correct : "optionC"
+    },
+];
